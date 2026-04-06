277ec42a9797944fac189e9971ec67f119330e75
Amayrajyadav <alladiamayrajyadav@gmail.com>
2026-04-06 19:40:48 +0530

    Add files via upload

---
+++ server/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from env.environment import DataCleaningEnv
+import uvicorn
+
+app = FastAPI()
+env = DataCleaningEnv()
+
+@app.post("/reset")
+def reset():
+    return env.reset()
+
+@app.post("/step")
+def step(action: dict):
+    # Ensure task exists
+    if env.current_task is None:
+        env.reset()
+
+    result = env.step(action)
+    return result
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
